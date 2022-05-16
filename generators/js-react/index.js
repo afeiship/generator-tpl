362343e7f3f949f8087be6e1929eb6f4532ddfb2
afeiship <1290657123@qq.com>
2022-05-17 00:14:06 +0800

    feat: tpl can debug

--- generators/js-react/index.js
+++ generators/js-react/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const Generator = require('yeoman-generator');
+const Generator = require('@jswork/yeoman-generator');
 const yosay = require('yosay');
 const globby = require('globby');
 const replace = require('replace-in-file');
