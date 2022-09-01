d5878d2b7b444a1225757bbd748fa27fc5bce537
afeiship <1290657123@qq.com>
2022-09-01 22:23:55 +0800

    feat: add routex-cli

---
+++ packages/routerx-cli/README.md
@@ -0,0 +1,46 @@
+# routerx-cli
+> Generate .routerrc file for react router.
+
+[![version][version-image]][version-url]
+[![license][license-image]][license-url]
+[![size][size-image]][size-url]
+[![download][download-image]][download-url]
+
+![snapshot](https://tva1.sinaimg.cn/large/0081Kckwgy1gk87ynhkbaj30u60asasp.jpg)
+
+## installation
+```shell
+# public
+npm i -g @jswork/routerx-cli
+
+# private
+git clone https://github.com/afeiship/routerx-cli.git
+cd routerx-cli
+npm i && npm link
+```
+
+## usage
+~~~
+Usage: routerx-cli [options]
+
+Options:
+  -V, --version       output the version number
+  -d, --debug         only show cmds, but not clean.
+  -s, --src <string>  source filepath. (default: "./src")
+  -h, --help          display help for command
+~~~
+
+## license
+Code released under [the MIT license](https://github.com/afeiship/routerx-cli/blob/master/LICENSE.txt).
+
+[version-image]: https://img.shields.io/npm/v/@jswork/routerx-cli
+[version-url]: https://npmjs.org/package/@jswork/routerx-cli
+
+[license-image]: https://img.shields.io/npm/l/@jswork/routerx-cli
+[license-url]: https://github.com/afeiship/routerx-cli/blob/master/LICENSE.txt
+
+[size-image]: https://img.shields.io/bundlephobia/minzip/@jswork/routerx-cli
+[size-url]: https://github.com/afeiship/routerx-cli/blob/master/dist/routerx-cli.min.js
+
+[download-image]: https://img.shields.io/npm/dm/@jswork/routerx-cli
+[download-url]: https://www.npmjs.com/package/@jswork/routerx-cli
---
+++ packages/routerx-cli/bin/index.js
@@ -0,0 +1,114 @@
+#!/usr/bin/env node
+const { Command } = require('commander');
+const fs = require('fs');
+const dirTree = require('directory-tree');
+
+// next packages:
+require('@jswork/next');
+require('@jswork/next-absolute-package');
+
+const { version } = nx.absolutePackage();
+const program = new Command();
+const deepEach = (ctx, fn) => {
+  if (ctx.children) {
+    ctx.children.forEach((item, index) => {
+      fn(item, index, ctx);
+      if (item.children) deepEach(item, fn);
+    });
+  }
+};
+
+program.version(version);
+
+program
+  .option('-c, --cwd <string>', 'source filepath.', 'src/modules')
+  .parse(process.argv);
+
+nx.declare({
+  statics: {
+    init() {
+      const app = new this();
+      app.start();
+    }
+  },
+  methods: {
+    init() {
+      const target = dirTree(`./${program.cwd}`, { attributes: ['type'] });
+      this.files = target.children.filter((item) => item.type === 'directory');
+      this.enumFiles = { children: this.files };
+    },
+    start() {
+      const enumFiles = this.enumFiles;
+      const cwd = program.cwd;
+      // 将 _misc.tsx 开头的文件标记为null，不属于页面模块文件
+      deepEach(enumFiles, (item, index, parent) => {
+        if (item.type === 'file') {
+          // console.log(item);
+          if (item.name.startsWith('_') && !item.name.includes('__init__')) {
+            parent.children[index] = null;
+          }
+        }
+      });
+
+      // 结合上面步骤，清理 null 的文件
+      deepEach(enumFiles, (item, index, parent) => {
+        if (item && item.type === 'directory' && item.children.length > 0) {
+          item.children = item.children.filter((item) => item !== null);
+        }
+      });
+
+      // 在 files 上添加 routes 信息
+      // 1. 给根目录的文件夹添加 routes，因为根目录是文件夹，所以，没有 filepath
+      this.files.forEach((item) => {
+        item.routerPath = `/${item.name}`;
+        item.routerFilePath = null;
+        deepEach(item, (item1, idx, parent) => {
+          const isFile = item1.type === 'file';
+          const isInitFile = isFile && item1.name === '__init__.tsx';
+          const isIndexFile = isFile && item1.name === 'index.tsx';
+          if (isInitFile) {
+            if (!parent.routerFilePath) {
+              parent.routerFilePath = item1.path;
+              parent.routerPath = parent.name;
+              parent.children[idx] = null;
+            }
+          }
+
+          if (isIndexFile) {
+            item1.routerIndex = true;
+            item1.routerFilePath = item1.path;
+          }
+
+          if (!isIndexFile && !isInitFile) {
+            if (isFile) {
+              item1.routerFilePath = item1.path;
+              item1.routerPath = item1.name.split('.')[0];
+            }
+          }
+        });
+      });
+
+      // 2. 清理 children 中的 null
+      deepEach(enumFiles, (item, index, parent) => {
+        if (item && item.type === 'directory' && item.children.length > 0) {
+          item.children = item.children.filter((item) => item !== null);
+        }
+      });
+
+      // 3. 去掉 cwd，清理 type/name/path
+      deepEach(enumFiles, (item) => {
+        if (item.routerFilePath) {
+          item.routerFilePath = item.routerFilePath.slice(cwd.length);
+        }
+        delete item.type;
+        delete item.name;
+        delete item.path;
+      });
+
+      fs.writeFileSync(
+        `./${cwd}/.routerc.json`,
+        JSON.stringify(this.enumFiles, null, 2)
+      );
+    }
+  }
+});
